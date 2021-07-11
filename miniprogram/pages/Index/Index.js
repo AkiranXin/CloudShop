ffdd84129755c618c64091f681f0140e89057b03
W-Kinlon <1154577806@qq.com>
2021-07-11 10:19:15 +0800

    首页优化代码

--- miniprogram/pages/Index/Index.js
+++ miniprogram/pages/Index/Index.js
@@ -79,43 +79,13 @@ Page({
       inputVal:''
     })
   },
+
+  
   onLoad: function() {
-    this.getOpenId()
-    let that = this
-    db.collection('swiper').get({
-      success:function(res){
-        console.log('轮播图获取成功',res)
-        that.setData({
-          banner:res.data
-        })
-      },
-      fail:function(res){
-        console.log('轮播图获取失败',res)
-      },
-    })
-    db.collection('fenlei').get({
-      success:function(res){
-        console.log('分类获取成功',res)
-        that.setData({
-          fenlei:res.data
-        })
-      },
-      fail:function(res){
-        console.log('分类获取失败',res)
-      },
-    })
-    db.collection('product').get({
-      success:function(res){
-        console.log('商品获取成功',res)
-        that.setData({
-          product:res.data
-        })
-      },
-      fail:function(res){
-        console.log('商品获取失败',res)
-      },
-    })
+
   },
+
+
   onShow:function(e){
     let that = this
     db.collection('swiper').get({
